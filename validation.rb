c111a26779123472442f4e3b31045c887c78f4a6
Sergey Toy <toydestroyer@me.com>
2014-12-09 21:27:23 +0300

    upd

--- validation.rb
+++ validation.rb
@@ -1,6 +1,6 @@
 validate do
   Rails.logger.info(subnet)
-  if Subnet.where("subnet >>= ?", subnet.to_s).any?
-    errors.add(:included, "already included")
+  if Subnet.where("subnet >>= ?", "#{subnet}/#{subnet.instance_variable_get(:@mask_addr).to_s(2).count('1')}").any?
+    errors.add(:included, "Данная подсеть уже покрыта")
   end
 end
