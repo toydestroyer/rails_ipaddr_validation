76a3cb8e81651b0354b139600768b3a1dbda1085
Sergey Toy <toydestroyer@me.com>
2014-12-14 22:26:00 +0300

    update

--- lib/ipaddr_validator.rb
+++ lib/ipaddr_validator.rb
@@ -1,10 +1,7 @@
 class IpaddrValidator < ActiveModel::EachValidator
-  def self.valid?
-    Subnet.where("subnet >>= ?", "#{subnet}/#{subnet.instance_variable_get(:@mask_addr).to_s(2).count('1')}").any?
-  end
 
   def validate_each(record, attribute, value)
-    unless self.class.valid?(value, self.options)
+    unless record.class.where("subnet >>= ?", "#{record}/#{record.instance_variable_get(:@mask_addr).to_s(2).count('1')}").any?
       record.errors.add(attribute, options[:message] || :included)
     end
   end
